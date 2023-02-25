0ee7c9c0e6f9df7dc88fcbf5adbdbb33ab4326d9
MikeBorges <mikeborges96@gmail.com>
2023-02-25 16:24:25 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+var instagram = document.querySelector(".button-instagram");
+
+instagram.addEventListener('click', function() {
+    window.open("https://www.instagram.com/mikeborgess/", '_blank');
+});
+
+
+var linkedin = document.querySelector(".button-linkedin");
+
+linkedin.addEventListener('click', function() {
+    window.open("https://www.linkedin.com/in/mike-borges-3a6037191/", '_blank');
+});
+
+
+var github = document.querySelector(".button-github");
+
+github.addEventListener('click', function() {
+    window.open("https://github.com/MikeBorges", '_blank');
+});
